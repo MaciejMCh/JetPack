a113a55d1d7d5d8339d3c532d3e935bc9fb19e84
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-10-01 12:40:05 +0200

    Initial Commit

---
+++ Jetpack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jetpack
+//
+//  Created by Maciej Chmielewski on 01.10.2016.
+//  Copyright © 2016 Maciej Chmielewski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Jetpack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jetpack
+//
+//  Created by Maciej Chmielewski on 01.10.2016.
+//  Copyright © 2016 Maciej Chmielewski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
